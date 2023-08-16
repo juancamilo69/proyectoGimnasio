fc7fe4dbcdf6d27218ac3359a403bbb7f64bff11
unknown <SENA@TUNAPRCGEFSD048.FormacionSena.RED>
2023-08-16 17:46:06 -0500

    Se inició con la construcción del view inicio

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,24 +1,102 @@
+<!-- Custom Css -->
+<link rel="stylesheet" href="{{ asset('css/dashboard.css') }}">
+
+<!-- Defautl Css -->
+@extends('default')
+
+<!-- Bootstrap -->
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-HwwvtgBNo3bZJJLYd8oVXjrBZt8cqVSpeBNS5n7C8IVInixGAoxmnlMuBnhbgrkm" crossorigin="anonymous"></script>
+
 <x-app-layout>
     <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Dashboard') }}
+        <h2 class="texto-bienvenido font-semibold text-gray-800 leading-tight">
+            {{ __('Bienvenido/a ') }}{{ Auth::user()->name }}
         </h2>
     </x-slot>
 
+    <!-- Hero Section -->
+    <div class="conatiner-fluid">
+        <img src="{{ asset('images/banner/banner1.jpg') }}" alt="">
+    </div>
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900">
-                    {{ __("You're logged in!") }}
-
-                    <h4>Home Page</h4>
-                    <p>Este contenido es público</p>
-                    @role('administrador')
-                    <p>Solo lo va a ver el rol administrador</p>
-                    @endrole
-                    @role('cliente')
-                    <p>Solo lo va a ver el rol cliente</p>
-                    @endrole
+                <div class="row row-cols-1 row-cols-md-3 g-4">
+  <div class="col">
+    <div class="card h-100">
+      <div class="card-body">
+        <h5 class="card-title">Plan Mensual</h5>
+        <h2>75.000</h2>
+        <p class="card-text">Un mes. ¡Máximo rendimiento!</p>
+      </div>
+      <div class="card-footer">
+        <small class="text-body-secondary">Last updated 3 mins ago</small>
+      </div>
+    </div>
+  </div>
+  <div class="col">
+    <div class="card h-100">
+      <div class="card-body">
+        <h5 class="card-title">Plan Bimestral</h5>
+        <h2>130.000</h2>
+        <p class="card-text">Dos meses. ¡Máximo rendimiento!</p>
+      </div>
+      <div class="card-footer">
+        <small class="text-body-secondary">Last updated 3 mins ago</small>
+      </div>
+    </div>
+  </div>
+  <div class="col">
+    <div class="card h-100">
+      <div class="card-body">
+        <h5 class="card-title">Plan Trimestral</h5>
+        <h2>195.000</h2>
+        <p class="card-text">Tres meses. ¡Máximo rendimiento!</p>
+      </div>
+      <div class="card-footer">
+        <small class="text-body-secondary">Last updated 3 mins ago</small>
+      </div>
+    </div>
+  </div>
+  <div class="col">
+    <div class="card h-100">
+      <div class="card-body">
+        <h5 class="card-title">Plan Semestral</h5>
+        <h2>390.000</h2>
+        <p class="card-text">Seis meses. ¡Máximo rendimiento!</p>
+      </div>
+      <div class="card-footer">
+        <small class="text-body-secondary">Last updated 3 mins ago</small>
+      </div>
+    </div>
+  </div>
+  <div class="col">
+    <div class="card h-100">
+      <div class="card-body">
+        <h5 class="card-title">Plan Anual</h5>
+        <h2>760.000</h2>
+        <p class="card-text">Un año. ¡Máximo rendimiento!</p>
+      </div>
+      <div class="card-footer">
+        <small class="text-body-secondary">Last updated 3 mins ago</small>
+      </div>
+    </div>
+  </div>
+  <div class="col">
+    <div class="card h-100">
+      <div class="card-body">
+        <h5 class="card-title">Plan Pareja</h5>
+        <h2>130.000</h2>
+        <p class="card-text">Un mes para pareja ¡Máximo rendimiento!</p>
+      </div>
+      <div class="card-footer">
+        <small class="text-body-secondary">Last updated 3 mins ago</small>
+      </div>
+    </div>
+  </div>
+</div>
                 </div>
             </div>
         </div>
---
+++ resources/views/default.blade.php
@@ -0,0 +1,4 @@
+<!-- Fonts -->
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@700&display=swap" rel="stylesheet">
